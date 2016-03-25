3b25b8fa7fef029030b8ca01fd42901c24ec7995
Connor Cummings <cpccummings@gmail.com>
2016-03-25 12:00:47 -0700

    Add validation border error for invalid form inputs

--- js/script.js
+++ js/script.js
@@ -1,15 +1,76 @@
 var score = 0;
-var checkInputs = function(inputNumber){
+var checkOne = function(inputNumber){
+  if( input.toUpperCase() === "HOT"){
+    score++;
+  }else if(inputNumber.toUpperCase() === "COLD"){
+    score+=1;
+    alert("cold");
+  }else{
+    alert("Whoops, it looks like we are missing something!");
+  }
+}
+var checkOne = function(inputNumber){
+  if( inputNumber.toUpperCase() === "HIGH"){
+    score+=2;
+    alert("high");
+  }else if(inputNumber.toUpperCase() === "LOW"){
+    alert("low");
+    score+=1;
+  }else{
+    alert("Whoops, it looks like we are missing something!");
+  }
+}
+var checkOne = function(inputNumber){
+  if( inputNumber.toUpperCase() === "MORNING"){
+    score+=2;
+    alert("morning");
+  }else if(inputNumber.toUpperCase() === "NIGHT"){
+    alert("night");
+    score+=1;
+  }else{
+    alert("Whoops, it looks like we are missing something!");
+  }
+}
+var checkOne = function(inputNumber){
+  if( inputNumber.toUpperCase() === "DRY"){
+    alert("dry");
+    score+=2;
+  }else if(inputNumber.toUpperCase() === "WET"){
+    alert("wet");
+    score+=1;
+  }else{
+    alert("Whoops, it looks like we are missing something!");
+  }
+}
+var checkOne = function(inputNumber){
   if( inputNumber.toUpperCase() === "HOT"){
     alert("hot");
-  }else if(inputNumber.toUpperCase() === "Cold"){
+    score+=2;
+  }else if(inputNumber.toUpperCase() === "COLD"){
+    alert("cold");
+    score+=1;
+  }else{
+    alert("Whoops, it looks like we are missing something!");
+  }
+}
+var checkOne = function(inputNumber){
+  if( inputNumber.toUpperCase() === "HOT"){
+    alert("hot");
+    score+=2;
+  }else if(inputNumber.toUpperCase() === "COLD"){
+    alert("cold")
+  }else{
+    alert("Whoops, it looks like we are missing something!");
+  }
+}
+var checkOne = function(inputNumber){
+  if( inputNumber.toUpperCase() === "HOT"){
+    alert("hot");
+  }else if(inputNumber.toUpperCase() === "COLD"){
     alert("cold")
   }else{
     alert("please fill in all missing");
   }
-
-
-
 }
 
 $(document).ready(function(){
