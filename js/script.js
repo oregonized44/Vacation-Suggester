16c5e457e30ec92d1ee69c9d806ed9b6d1f85ffa
Connor Cummings <cpccummings@gmail.com>
2016-03-25 10:43:12 -0700

    Add value to form function and alert asnwers

--- js/script.js
+++ js/script.js
@@ -1,14 +1,47 @@
+
+
 $(document).ready(function(){
+
+
+
+
+
+
+
+
+var inputSix = $("input:radio[name=coding]:checked").val();
+
   $("#button").click(function(){
     alert("button clicked");
+    var inputOne = $("#questionOne").val();
+    alert(inputOne);
+    var inputTwo = $("#questionTwo").val();
+    alert(inputTwo);
+    var inputThree = $("#questionThree").val();
+    alert(inputThree);
+    var inputFour = $("#questionFour").val();
+    alert(inputFour);
+    var inputFive = $("#questionFive").val();
+    alert(inputFive)
+    var inputSix = $("#questionSix").val();
+    alert(inputSix);
 
   });
+
+
+
+
+
+
+
+
+
+
   $(".click-me").click(function(){
     alert("getting ready to hide");
     $(this).hide();
     $("h1").hide();
     $("#form").show();
-
   });
 
 });
