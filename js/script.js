32b04f84260a04820950c7d12d10310fa52df92a
Connor Cummings <cpccummings@gmail.com>
2016-03-25 13:17:02 -0700

    Collect scores by collecting function object's score during form input.

--- js/script.js
+++ js/script.js
@@ -1,95 +1,110 @@
 var score = 0;
 var checkOne = function(inputNumber){
-  if( input.toUpperCase() === "HOT"){
-    score++;
+  if( inputNumber.toUpperCase() === "HOT"){
+    checkOne.score = 2;
+    console.log(checkOne.score);
   }else if(inputNumber.toUpperCase() === "COLD"){
-    score+=1;
-    alert("cold");
+    checkOne.score = 1;
+    console.log(checkOne.score);
   }else{
-    alert("Whoops, it looks like we are missing something!");
+    alert("Whoops, it looks like we are missing something! Check question 1");
   }
 }
-var checkOne = function(inputNumber){
+var checkTwo = function(inputNumber){
   if( inputNumber.toUpperCase() === "HIGH"){
-    score+=2;
+    checkTwo.score = 2;
+
     alert("high");
   }else if(inputNumber.toUpperCase() === "LOW"){
     alert("low");
-    score+=1;
+
+    checkTwo.score = 1;
   }else{
-    alert("Whoops, it looks like we are missing something!");
+
+    alert("Whoops, it looks like we are missing something! Check question2");
   }
 }
-var checkOne = function(inputNumber){
+var checkThree = function(inputNumber){
   if( inputNumber.toUpperCase() === "MORNING"){
-    score+=2;
+    checkThree.score = 2;
+
     alert("morning");
   }else if(inputNumber.toUpperCase() === "NIGHT"){
     alert("night");
-    score+=1;
+
+    checkThree.score = 1;
   }else{
-    alert("Whoops, it looks like we are missing something!");
+
+    alert("Whoops, it looks like we are missing something! Check question 3");
   }
 }
-var checkOne = function(inputNumber){
+var checkFour = function(inputNumber){
   if( inputNumber.toUpperCase() === "DRY"){
     alert("dry");
-    score+=2;
+
+    checkFour.score = 2;
   }else if(inputNumber.toUpperCase() === "WET"){
     alert("wet");
-    score+=1;
+
+    checkFour.score = 1;
   }else{
-    alert("Whoops, it looks like we are missing something!");
+
+    alert("Whoops, it looks like we are missing something! Check question 4");
   }
 }
-var checkOne = function(inputNumber){
+var checkFive = function(inputNumber){
   if( inputNumber.toUpperCase() === "HOT"){
     alert("hot");
-    score+=2;
+
+    checkFive.score = 2;
   }else if(inputNumber.toUpperCase() === "COLD"){
     alert("cold");
-    score+=1;
+
+    checkFive.score = 1;
   }else{
-    alert("Whoops, it looks like we are missing something!");
+
+    alert("Whoops, it looks like we are missing something! Check question 5");
   }
 }
-var checkOne = function(inputNumber){
+var checkSix = function(inputNumber){
   if( inputNumber.toUpperCase() === "HOT"){
     alert("hot");
-    score+=2;
+
+    checkSix.score = 2;
   }else if(inputNumber.toUpperCase() === "COLD"){
     alert("cold")
+
+    checkSix.score = 1;
   }else{
-    alert("Whoops, it looks like we are missing something!");
-  }
-}
-var checkOne = function(inputNumber){
-  if( inputNumber.toUpperCase() === "HOT"){
-    alert("hot");
-  }else if(inputNumber.toUpperCase() === "COLD"){
-    alert("cold")
-  }else{
-    alert("please fill in all missing");
+
+    alert("Whoops, it looks like we are missing something! Check question 6");
   }
 }
 
 $(document).ready(function(){
-  $("#button").click(function(){
-    alert("button clicked");
+  $("#button").click(function(event){
+    console.log("button clicked");
+    event.defaultPrevent;
     var inputOne = $("#questionOne").val();
-    alert(inputOne);
+    console.log(inputOne);
     var inputTwo = $("#questionTwo").val();
-    alert(inputTwo);
+    console.log(inputTwo);
     var inputThree = $("#questionThree").val();
-    alert(inputThree);
+    console.log(inputThree);
     var inputFour = $("#questionFour").val();
-    alert(inputFour);
+    console.log(inputFour);
     var inputFive = $("#questionFive").val();
-    alert(inputFive)
+    console.log(inputFive)
     var inputSix = $("#questionSix").val();
-    alert(inputSix);
-    checkInputs(inputOne);
-
+    console.log(inputSix);
+    checkOne(inputOne);
+    checkTwo(inputTwo);
+    checkThree(inputThree);
+    checkFour(inputFour);
+    checkFive(inputFive);
+    checkSix(inputSix);
+    var score = checkOne.score + checkTwo.score);
+    vacation();
 
 
   });
