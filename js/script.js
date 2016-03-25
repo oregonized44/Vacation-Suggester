eec3c5f4190f6c88cf5e6ba001217ebdd2a47db6
Connor Cummings <cpccummings@gmail.com>
2016-03-25 14:11:26 -0700

    add extra functionality with a case switch operation to link vacations sites

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
-var score = 0;
+//////////////////////////////////////////////////////////////////////////////
+//CHECK ONE///////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
 var checkOne = function(inputNumber){
   if( inputNumber.toUpperCase() === "HOT"){
     checkOne.score = 2;
@@ -10,101 +13,122 @@ var checkOne = function(inputNumber){
     alert("Whoops, it looks like we are missing something! Check question 1");
   }
 }
+
+//////////////////////////////////////////////////////////////////////////////
+//CHECK TWO///////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
 var checkTwo = function(inputNumber){
   if( inputNumber.toUpperCase() === "HIGH"){
     checkTwo.score = 2;
-
-    alert("high");
   }else if(inputNumber.toUpperCase() === "LOW"){
-    alert("low");
-
     checkTwo.score = 1;
   }else{
-
     alert("Whoops, it looks like we are missing something! Check question2");
   }
 }
+
+//////////////////////////////////////////////////////////////////////////////
+//CHECK THREE/////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
 var checkThree = function(inputNumber){
   if( inputNumber.toUpperCase() === "MORNING"){
     checkThree.score = 2;
-
-    alert("morning");
   }else if(inputNumber.toUpperCase() === "NIGHT"){
-    alert("night");
-
     checkThree.score = 1;
   }else{
-
     alert("Whoops, it looks like we are missing something! Check question 3");
   }
 }
+
+//////////////////////////////////////////////////////////////////////////////
+//CHECK FOUR//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
 var checkFour = function(inputNumber){
   if( inputNumber.toUpperCase() === "DRY"){
-    alert("dry");
-
     checkFour.score = 2;
   }else if(inputNumber.toUpperCase() === "WET"){
-    alert("wet");
-
     checkFour.score = 1;
   }else{
-
     alert("Whoops, it looks like we are missing something! Check question 4");
   }
 }
+
+//////////////////////////////////////////////////////////////////////////////
+//CHECK FIVE//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
 var checkFive = function(inputNumber){
   if( inputNumber.toUpperCase() === "HOT"){
-    alert("hot");
-
     checkFive.score = 2;
   }else if(inputNumber.toUpperCase() === "COLD"){
-    alert("cold");
-
     checkFive.score = 1;
   }else{
-
     alert("Whoops, it looks like we are missing something! Check question 5");
   }
 }
+
+//////////////////////////////////////////////////////////////////////////////
+//CHECK SIX////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
 var checkSix = function(inputNumber){
-  if( inputNumber.toUpperCase() === "HOT"){
+  if( inputNumber.toUpperCase() === "SUMMER" || inputNumber.toUpperCase() === "SPRING"){
     alert("hot");
-
     checkSix.score = 2;
-  }else if(inputNumber.toUpperCase() === "COLD"){
-    alert("cold")
-
+  }else if(inputNumber.toUpperCase() === "FALL" || inputNumber.toUpperCase() === "WINTER"){
     checkSix.score = 1;
   }else{
-
     alert("Whoops, it looks like we are missing something! Check question 6");
   }
 }
 
+//////////////////////////////////////////////////////////////////////////////
+//EVENT LISTENERS/////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
 $(document).ready(function(){
   $("#button").click(function(event){
-    console.log("button clicked");
+
     event.defaultPrevent;
     var inputOne = $("#questionOne").val();
-    console.log(inputOne);
     var inputTwo = $("#questionTwo").val();
-    console.log(inputTwo);
     var inputThree = $("#questionThree").val();
-    console.log(inputThree);
     var inputFour = $("#questionFour").val();
-    console.log(inputFour);
     var inputFive = $("#questionFive").val();
-    console.log(inputFive)
     var inputSix = $("#questionSix").val();
-    console.log(inputSix);
     checkOne(inputOne);
     checkTwo(inputTwo);
     checkThree(inputThree);
     checkFour(inputFour);
     checkFive(inputFive);
     checkSix(inputSix);
-    var score = checkOne.score + checkTwo.score);
-    vacation();
+    var score = checkOne.score + checkTwo.score + checkThree.score + checkFour.score + checkFive.score + checkSix.score;
+    alert(score);
+    switch(score){
+      case 6: alert("winner 6");
+      break;
+      case 7: alert("winner 7");
+      break;
+      case 8: alert("winner8");
+      break;
+      case 9: alert("winner 9");
+      break;
+      case 10: alert("winner10");
+      break;
+      case 11: alert("winner 11");
+      break;
+      case 12: alert("winner 12");
+      break;
+    }
+
+
+
+  });
+
+  $(".letsTravel").click(function(){
 
 
   });
@@ -117,9 +141,7 @@ $(document).ready(function(){
 
 
 
-
   $(".click-me").click(function(){
-    alert("getting ready to hide");
     $(this).hide();
     $("h1").hide();
     $("#form").show();
